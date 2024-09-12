f17e47039a4df56dc5625c75d1fb3fb818eeedfd
mdaugus <marcio.augusto@simples.software>
2024-09-12 03:33:40 -0300

    # Prototipo V1

---
+++ resources/js/datatable/pt_BR.js
@@ -0,0 +1,25 @@
+var localeJsonDataTable = {
+	"sEmptyTable":   	"Não existem dados para serem exibidos",
+	"sInfo":         	"_START_ a _END_ de _TOTAL_ registros",
+	"sInfoEmpty":    	"0 a 0 de 0 registros",
+	"sInfoFiltered": 	" ",
+	"sInfoPostFix":  	"",
+	"sInfoThousands":  	".",
+	"sInfoDecimals": 			",",
+	"sLengthMenu":   	"Exibir _MENU_ registros por página",
+	"sLoadingRecords": 	"Carregando...",
+	"sProcessing":   	"Processando...",
+	"search": "_INPUT_",
+    "searchPlaceholder": "Pesquisar",
+	"sZeroRecords":  	"Não existem dados para serem exibidos",
+	"oPaginate": {
+		"sFirst":    	"Primeira",
+		"sPrevious": 	"Anterior",
+		"sNext":     	"Próxima",
+		"sLast":     	"Última"
+	},
+	"oAria": {
+		"sSortAscending":  ": aktivieren, um Spalte aufsteigend zu sortieren",
+		"sSortDescending": ": aktivieren, um Spalte absteigend zu sortieren"
+	}
+}

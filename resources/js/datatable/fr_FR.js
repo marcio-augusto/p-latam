35d6e03685683adbe47dfbafcc7da56659b6d54f
mdaugus <marcio.augusto@simples.software>
2025-10-14 19:13:22 -0300

    Versão MRO 1.0

---
+++ resources/js/datatable/fr_FR.js
@@ -0,0 +1,23 @@
+var localeJsonDataTable = {
+	"sEmptyTable":   	"Não existem dados para serem exibidos",
+	"sInfo":         	"_START_ a _END_ de _TOTAL_ registros",
+	"sInfoEmpty":    	"0 a 0 de 0 registros",
+	"sInfoFiltered": 	" ",
+	"sInfoPostFix":  	"",
+	"sInfoThousands":  	".",
+	"sLengthMenu":   	"Em espanhol _MENU_ registros por página",
+	"sLoadingRecords": 	"Carregando...",
+	"sProcessing":   	"Processando...",
+	"sSearch":       	"Procurar por WO:  ",
+	"sZeroRecords":  	"Não existem dados para serem exibidos",
+	"oPaginate": {
+		"sFirst":    	"Primeira",
+		"sPrevious": 	"Anterior",
+		"sNext":     	"Próxima",
+		"sLast":     	"Última"
+	},
+	"oAria": {
+		"sSortAscending":  ": aktivieren, um Spalte aufsteigend zu sortieren",
+		"sSortDescending": ": aktivieren, um Spalte absteigend zu sortieren"
+	}
+}
